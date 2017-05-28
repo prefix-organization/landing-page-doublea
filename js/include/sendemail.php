87e56aa09bb72585a05657604f9196424f3b983c
marc <mc-georges@live.fr>
2017-05-28 03:24:03 +0200

    mis en place landing page dba

---
+++ js/include/sendemail.php
@@ -0,0 +1,88 @@
+<?php
+
+require_once('phpmailer/PHPMailerAutoload.php');
+
+$toemails = array();
+
+$toemails[0] = array(
+				'email' => 'marc.crucifix@prefix.fr', // Your Email Address
+				'name' => 'Marc' // Your Name
+			);
+
+$toemails[1] = array(
+				'email' => 'olivier.dupre@prefix.fr', // Your Email Address
+				'name' => 'Olivier' // Your Name
+			);
+
+$toemails[2] = array(
+				'email' => 'contact@prefix.fr', // Your Email Address
+				'name' => 'Contact' // Your Name
+			);
+
+// Form Processing Messages
+$message_success = 'response-ok';
+
+// Add this only if you use reCaptcha with your Contact Forms
+$recaptcha_secret = ''; // Your reCaptcha Secret
+
+$mail = new PHPMailer();
+
+// If you intend you use SMTP, add your SMTP Code after this Line
+
+
+if( $_SERVER['REQUEST_METHOD'] == 'POST' ) {
+	if( $_POST['template-contactform-email'] != '' ) {
+
+		$name = isset( $_POST['template-contactform-name'] ) ? $_POST['template-contactform-name'] : '';
+		$email = isset( $_POST['template-contactform-email'] ) ? $_POST['template-contactform-email'] : '';
+		$phone = isset( $_POST['template-contactform-phone'] ) ? $_POST['template-contactform-phone'] : '';
+		$subject = isset( $_POST['template-contactform-subject'] ) ? $_POST['template-contactform-subject'] : '';
+		$message = isset( $_POST['template-contactform-message'] ) ? $_POST['template-contactform-message'] : '';
+
+		$subject = isset($subject) ? $subject : 'New Message From Contact Form';
+
+		$botcheck = $_POST['template-contactform-botcheck'];
+
+		if( $botcheck == '' ) {
+
+			$mail->SetFrom( $email , $name );
+			$mail->AddReplyTo( $email , $name );
+			foreach( $toemails as $toemail ) {
+				$mail->AddAddress( $toemail['email'] , $toemail['name'] );
+			}
+			$mail->Subject = $subject;
+
+			$name = isset($name) ? "Name: $name<br><br>" : '';
+			$email = isset($email) ? "Email: $email<br><br>" : '';
+			$phone = isset($phone) ? "Phone: $phone<br><br>" : '';
+			$message = isset($message) ? "Message: $message<br><br>" : '';
+
+			$referrer = $_SERVER['HTTP_REFERER'] ? '<br><br><br>This Form was submitted from: ' . $_SERVER['HTTP_REFERER'] : '';
+
+			$body = "$name $email $phone $message $referrer";
+
+			// Runs only when File Field is present in the Contact Form
+			if ( isset( $_FILES['template-contactform-file'] ) && $_FILES['template-contactform-file']['error'] == UPLOAD_ERR_OK ) {
+				$mail->IsHTML(true);
+				$mail->AddAttachment( $_FILES['template-contactform-file']['tmp_name'], $_FILES['template-contactform-file']['name'] );
+			}
+
+			$mail->MsgHTML( $body );
+			$sendEmail = $mail->Send();
+
+			if( $sendEmail == true ):
+				echo $message_success;
+			else:
+				echo '{ "alert": "error", "message": "Email <strong>could not</strong> be sent due to some Unexpected Error. Please Try Again later.<br /><br /><strong>Reason:</strong><br />' . $mail->ErrorInfo . '" }';
+			endif;
+		} else {
+			echo '{ "alert": "error", "message": "Bot <strong>Detected</strong>.! Clean yourself Botster.!" }';
+		}
+	} else {
+		echo '{ "alert": "error", "message": "Please <strong>Fill up</strong> all the Fields and Try Again." }';
+	}
+} else {
+	echo '{ "alert": "error", "message": "An <strong>unexpected error</strong> occured. Please Try Again later." }';
+}
+
+?>
